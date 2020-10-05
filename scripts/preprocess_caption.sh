0da046e06a46f1c9d8f518c403a9a872bdd920c8
kplbagre <kplbagre07@gmail.com>
2020-10-05 16:38:16 +0530

    Update preprocess_caption.sh

--- scripts/preprocess_caption.sh
+++ scripts/preprocess_caption.sh
@@ -1,11 +1,11 @@
-. ./CONFIG
+/content/tagan/CONFIG
 
-python preprocess_caption.py \
+python /content/tagan/preprocess_caption.py \
     --caption_root ${FLOWERS_CAPTION_ROOT} \
     --fasttext_model ${FASTTEXT_MODEL} \
     --max_nwords 50
 
-python preprocess_caption.py \
+python /content/tagan/preprocess_caption.py \
     --caption_root ${BIRDS_CAPTION_ROOT} \
     --fasttext_model ${FASTTEXT_MODEL} \
     --max_nwords 50
