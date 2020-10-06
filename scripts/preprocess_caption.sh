1bf94d7bc4a3710650a8c0c73c2f35a399acd94d
kplbagre <kplbagre07@gmail.com>
2020-10-06 12:14:06 +0530

    Update preprocess_caption.sh

--- scripts/preprocess_caption.sh
+++ scripts/preprocess_caption.sh
@@ -1,11 +1,11 @@
-/content/tagan/CONFIG
+/content/colab-tagan/CONFIG
 
-python /content/tagan/preprocess_caption.py \
+python /content/colab-tagan/preprocess_caption.py \
     --caption_root ${FLOWERS_CAPTION_ROOT} \
     --fasttext_model ${FASTTEXT_MODEL} \
     --max_nwords 50
 
-python /content/tagan/preprocess_caption.py \
+python /content/colab-tagan/preprocess_caption.py \
     --caption_root ${BIRDS_CAPTION_ROOT} \
     --fasttext_model ${FASTTEXT_MODEL} \
     --max_nwords 50
