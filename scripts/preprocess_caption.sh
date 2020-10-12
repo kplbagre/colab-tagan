22ef04d4d5d791b8e1de536e50843f35f9f883a3
kplbagre <kplbagre07@gmail.com>
2020-10-12 10:41:36 +0530

    Update preprocess_caption.sh

--- scripts/preprocess_caption.sh
+++ scripts/preprocess_caption.sh
@@ -1,11 +1,11 @@
 source /content/colab-tagan/CONFIG
 
 python /content/colab-tagan/preprocess_caption.py \
-    --caption_root ${FLOWERS_CAPTION_ROOT} \
-    --fasttext_model ${FASTTEXT_MODEL} \
+    --caption_root /content/drive/'My Drive'/MTP_Dataset/oxford102/flowers_icml \
+    --fasttext_model /content/drive/'My Drive'/MTP_Dataset/fastText/wiki.en/wiki.en.bin \
     --max_nwords 50
 
 python /content/colab-tagan/preprocess_caption.py \
-    --caption_root ${BIRDS_CAPTION_ROOT} \
-    --fasttext_model ${FASTTEXT_MODEL} \
+    --caption_root /content/drive/'My Drive'/MTP_Dataset/CUB_200_2011/cub_icml \
+    --fasttext_model /content/drive/'My Drive'/MTP_Dataset/fastText/wiki.en/wiki.en.bin \
     --max_nwords 50
