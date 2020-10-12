06a7c04903771ff120f1d676044f2950f6c7fede
kplbagre <kplbagre07@gmail.com>
2020-10-12 23:38:54 +0530

    Update test_birds.sh

--- scripts/test_birds.sh
+++ scripts/test_birds.sh
@@ -1,8 +1,8 @@
 source /content/colab-tagan/CONFIG
 
 python /content/colab-tagan/test.py \
-    --img_root /content/drive/'My Drive'/MTP_Dataset/CUB_200_2011/images \
+    --img_root /content/drive/'My Drive'/MTP_Dataset/CUB_200_2011/images/149.Brown_Thrasher \
     --text_file /content/colab-tagan/test/text_birds.txt \
     --fasttext_model /content/drive/'My Drive'/MTP_Dataset/fastText/wiki.en/wiki.en.bin \
     --generator_model /content/drive/'My Drive'/MTP_Dataset/models/birds_G.pth \
-    --output_root /content/colab-tagan/test/result_birds
+    --output_root /content/drive/'My Drive'/MTP_Dataset/output/result_birds
