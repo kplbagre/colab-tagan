93f91910e0c0f9bfaee668ee7a307ee4f9a8f472
kplbagre <kplbagre07@gmail.com>
2020-12-13 16:00:38 +0530

    Update train_flowers.sh

--- scripts/train_flowers.sh
+++ scripts/train_flowers.sh
@@ -3,7 +3,7 @@ source /content/colab-tagan/CONFIG
 python /content/colab-tagan/train.py \
     --img_root /content/drive/'My Drive'/MTP_Dataset/oxford102/jpg \
     --caption_root /content/drive/'My Drive'/MTP_Dataset/oxford102/flowers_icml \
-    --trainclasses_file /content/drive/'My Drive'/MTP_Dataset/oxford102/flowers_icml/trainvalclasses.txt \
+    --trainclasses_file /content/drive/'My Drive'/MTP_Dataset/oxford102/flowers_icml_vec/trainvalclasses.txt \
     --save_filename_G /content/drive/'My Drive'/MTP_Dataset/models/flowers_G.pth \
     --save_filename_D /content/drive/'My Drive'/MTP_Dataset/models/flowers_D.pth \
     --lambda_cond_loss 10 \
