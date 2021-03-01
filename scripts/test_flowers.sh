92efb941f31dd4f88e96bfec8f15f810eb480821
kplbagre <kplbagre07@gmail.com>
2021-03-01 09:18:40 +0530

    Update test_flowers.sh

--- scripts/test_flowers.sh
+++ scripts/test_flowers.sh
@@ -1,8 +1,8 @@
-source /content/colab-tagan/CONFIG
+source /others/cs19m029/colab-tagan/CONFIG
 
-python /content/colab-tagan/test.py \
-    --img_root /content/drive/'My Drive'/MTP_Dataset/oxford102/flowers_test \
-    --text_file /content/colab-tagan/test/text_flowers.txt \
-    --fasttext_model /content/drive/'My Drive'/MTP_Dataset/fastText/wiki.en/wiki.en.bin \
-    --generator_model /content/drive/'My Drive'/MTP_Dataset/models/flowers_G.pth \
-    --output_root /content/drive/'My Drive'/MTP_Dataset/output/result_flowers
+python /others/cs19m029/colab-tagan/test.py \
+    --img_root /others/cs19m029/MTP_Dataset/oxford102/flowers_test \
+    --text_file /others/cs19m029/colab-tagan/test/text_flowers.txt \
+    --fasttext_model /others/cs19m029/MTP_Dataset/fastText/wiki.en/wiki.en.bin \
+    --generator_model /others/cs19m029/MTP_Dataset/models/flowers_G.pth \
+    --output_root /others/cs19m029/MTP_Dataset/output/result_flowers
