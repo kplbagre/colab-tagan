f4d10b20f9c43d35bd0d7afcf5150faf2116bb9b
kplbagre <kplbagre07@gmail.com>
2021-03-01 08:53:51 +0530

    Update preprocess_caption.sh

--- scripts/preprocess_caption.sh
+++ scripts/preprocess_caption.sh
@@ -1,11 +1,11 @@
-source /content/colab-tagan/CONFIG
+source /others/cs19m029/colab-tagan/CONFIG
 
-python /content/colab-tagan/preprocess_caption.py \
-    --caption_root /content/drive/'My Drive'/MTP_Dataset/oxford102/flowers_icml_vec \
-    --fasttext_model /content/drive/'My Drive'/MTP_Dataset/fastText/wiki.en/wiki.en.bin \
+python /others/cs19m029/colab-tagan/preprocess_caption.py \
+    --caption_root /others/cs19m029/MTP_Dataset/oxford102/flowers_icml_vec \
+    --fasttext_model /others/cs19m029/MTP_Dataset/fastText/wiki.en/wiki.en.bin \
     --max_nwords 50
 
-python /content/colab-tagan/preprocess_caption.py \
-    --caption_root /content/drive/'My Drive'/MTP_Dataset/CUB_200_2011/cub_icml \
-    --fasttext_model /content/drive/'My Drive'/MTP_Dataset/fastText/wiki.en/wiki.en.bin \
+python /others/cs19m029/colab-tagan/preprocess_caption.py \
+    --caption_root /others/cs19m029/MTP_Dataset/CUB_200_2011/cub_icml \
+    --fasttext_model /others/cs19m029/MTP_Dataset/fastText/wiki.en/wiki.en.bin \
     --max_nwords 50
