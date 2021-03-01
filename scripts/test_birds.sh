2bc2621ac6ecdcad6ab121caf8b7e8e08c335f13
kplbagre <kplbagre07@gmail.com>
2021-03-01 09:18:13 +0530

    Update test_birds.sh

--- scripts/test_birds.sh
+++ scripts/test_birds.sh
@@ -1,8 +1,8 @@
-source /content/colab-tagan/CONFIG
+source /others/cs19m029/colab-tagan/CONFIG
 
-python /content/colab-tagan/test.py \
-    --img_root /content/drive/'My Drive'/MTP_Dataset/CUB_200_2011/bird_test \
-    --text_file /content/colab-tagan/test/text_birds.txt \
-    --fasttext_model /content/drive/'My Drive'/MTP_Dataset/fastText/wiki.en/wiki.en.bin \
-    --generator_model /content/drive/'My Drive'/MTP_Dataset/models/birds_G.pth \
-    --output_root /content/drive/'My Drive'/MTP_Dataset/output/result_birds
+python /others/cs19m029/colab-tagan/test.py \
+    --img_root /others/cs19m029/MTP_Dataset/CUB_200_2011/bird_test \
+    --text_file /others/cs19m029/colab-tagan/test/text_birds.txt \
+    --fasttext_model /others/cs19m029/MTP_Dataset/fastText/wiki.en/wiki.en.bin \
+    --generator_model /others/cs19m029/MTP_Dataset/models/birds_G.pth \
+    --output_root /others/cs19m029/MTP_Dataset/output/result_birds
