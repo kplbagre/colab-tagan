431bbc7e1e3f07c768d35b39bcc335483d6b92c8
kplbagre <kplbagre07@gmail.com>
2021-03-01 09:37:05 +0530

    Update train_flowers.sh

--- scripts/train_flowers.sh
+++ scripts/train_flowers.sh
@@ -4,7 +4,7 @@ python /others/cs19m029/colab-tagan/train.py \
     --img_root /others/cs19m029/MTP_Dataset/oxford102/jpg \
     --caption_root /others/cs19m029/MTP_Dataset/oxford102/flowers_icml_vec \
     --trainclasses_file /others/cs19m029/MTP_Dataset/oxford102/flowers_icml_vec/trainvalclasses.txt \
-    --save_filename_G /others/cs19m029/MTP_Dataset/models/flowers_Train.pth \
-    --save_filename_D /others/cs19m029/MTP_Dataset/models/flowers_Train.pth \
+    --save_filename_G /others/cs19m029/MTP_Dataset/models/flowers_Train_G.pth \
+    --save_filename_D /others/cs19m029/MTP_Dataset/models/flowers_Train_D.pth \
     --lambda_cond_loss 10 \
     --lambda_recon_loss 0.2
