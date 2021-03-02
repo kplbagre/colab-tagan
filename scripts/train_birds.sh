11c60b79d857233bdc444690d8cf639174c3a70a
kplbagre <kplbagre07@gmail.com>
2021-03-02 11:02:14 +0530

    Update train_birds.sh

--- scripts/train_birds.sh
+++ scripts/train_birds.sh
@@ -2,7 +2,7 @@ source /others/cs19m029/colab-tagan/CONFIG
 
 python /others/cs19m029/colab-tagan/train.py \
     --img_root /others/cs19m029/MTP_Dataset/CUB_200_2011/images \
-    --caption_root /others/cs19m029/MTP_Dataset/CUB_200_2011/cub_icml_vec/ \
+    --caption_root /others/cs19m029/MTP_Dataset/CUB_200_2011/cub_icml_vec \
     --trainclasses_file /others/cs19m029/MTP_Dataset/CUB_200_2011/cub_icml_vec/trainclasses.txt \
     --save_filename_G /others/cs19m029/MTP_Dataset/models/birds_train_G.pth \
     --save_filename_D /others/cs19m029/MTP_Dataset/models/birds_train_D.pth \
