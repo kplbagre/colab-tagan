a85935ce6a15056303fce799464b0709dc68d583
kplbagre <kplbagre07@gmail.com>
2021-03-08 22:38:15 +0530

    Create test_flowers_new.sh

---
+++ scripts/test_flowers_new.sh
@@ -0,0 +1,8 @@
+source /content/colab-tagan/CONFIG
+
+python /content/colab-tagan/test.py \
+    --img_root /content/drive/'My Drive'/MTP_Dataset/oxford102/flowers_test \
+    --text_file /content/colab-tagan/test/text_flowers.txt \
+    --fasttext_model /content/drive/'My Drive'/MTP_Dataset/fastText/wiki.en/wiki.en.bin \
+    --generator_model /content/drive/'My Drive'/MTP_Dataset/models/flowers_Train_G.pth \
+    --output_root /content/drive/'My Drive'/MTP_Dataset/output/result_flowers
