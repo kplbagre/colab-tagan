926e0c36513a336ece0a6f2ef9fd62bdd13bea8b
kplbagre <kplbagre07@gmail.com>
2021-03-08 22:37:39 +0530

    Create test_birds_new.sh

---
+++ scripts/test_birds_new.sh
@@ -0,0 +1,8 @@
+source /content/colab-tagan/CONFIG
+
+python /content/colab-tagan/test.py \
+    --img_root /content/drive/'My Drive'/MTP_Dataset/CUB_200_2011/bird_test \
+    --text_file /content/colab-tagan/test/text_birds.txt \
+    --fasttext_model /content/drive/'My Drive'/MTP_Dataset/fastText/wiki.en/wiki.en.bin \
+    --generator_model /content/drive/'My Drive'/MTP_Dataset/models/birds_Train_G.pth \
+    --output_root /content/drive/'My Drive'/MTP_Dataset/output/result_birds
