7d466f817e5c07fa718eab3ab780d2f64664bcd2
kplbagre <kplbagre07@gmail.com>
2021-03-08 12:01:38 +0530

    Update test_flowers_new.sh

--- scripts/test_flowers_new.sh
+++ scripts/test_flowers_new.sh
@@ -5,4 +5,4 @@ python /others/cs19m029/colab-tagan/test.py \
     --text_file /others/cs19m029/colab-tagan/test/text_flowers.txt \
     --fasttext_model /others/cs19m029/MTP_Dataset/fastText/wiki.en/wiki.en.bin \
     --generator_model /others/cs19m029/MTP_Dataset/models/flowers_Train_G.pth \
-    --output_root /others/cs19m029/MTP_Dataset/output/result_flowers
+    --output_root /others/cs19m029/MTP_Dataset/output/result_flowers_new
