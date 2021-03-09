9b4311f3d9f6d941011c63d5b5f82df9bc8c868d
kplbagre <kplbagre07@gmail.com>
2021-03-09 09:54:53 +0530

    Update train_flowers.sh

--- scripts/train_flowers.sh
+++ scripts/train_flowers.sh
@@ -1,7 +1,7 @@
 source /others/cs19m029/colab-tagan/CONFIG
 
 python /others/cs19m029/colab-tagan/train.py \
-    --img_root /others/cs19m029/MTP_Dataset/oxford102/jpg \
+    --img_root /others/cs19m029/MTP_Dataset/oxford102 \
     --caption_root /others/cs19m029/MTP_Dataset/oxford102/flowers_icml \
     --trainclasses_file /others/cs19m029/MTP_Dataset/oxford102/flowers_icml/trainclasses.txt \
     --save_filename_G /others/cs19m029/MTP_Dataset/models/flowers_Train_G.pth \
